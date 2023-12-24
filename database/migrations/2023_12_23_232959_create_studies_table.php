51c312712211f0562ecc7ec7f9ee0480fd9d94f9
faniapriadi073 <faniapriadi073@gmail.com>
2023-12-24 09:34:53 +0800

    menambahkan tabel migration

---
+++ database/migrations/2023_12_23_232959_create_studies_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('studies', function (Blueprint $table) {
+            $table->id();
+            $table->integer('kelas_id');
+            $table->integer('subject_id');
+            $table->integer('teacher_id');
+            $table->integer('standard');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('studies');
+    }
+};
---
+++ database/migrations/2023_12_23_235448_create_final_values_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('final_values', function (Blueprint $table) {
+            $table->id();
+            $table->integer('student_id');
+            $table->float('knowledge');
+            $table->float('ability');
+            $table->float('pts');
+            $table->float('pas');
+            $table->float('average');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('final_values');
+    }
+};
