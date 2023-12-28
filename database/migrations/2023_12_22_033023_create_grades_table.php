2437f25726cb155fa739753ccf283a22a7cd1bc9
hamzandev <hamzankechil@gmail.com>
2023-12-28 09:00:00 +0800

    Burn Out boskuu

--- database/migrations/2023_12_22_033023_create_grades_table.php
+++ database/migrations/2023_12_22_033023_create_grades_table.php
@@ -18,7 +18,6 @@ return new class extends Migration
             $table->integer('teacher_id');
             $table->integer('subject_id');
             $table->double('value');
-            $table->double('weight');
             $table->timestamps();
         });
     }
